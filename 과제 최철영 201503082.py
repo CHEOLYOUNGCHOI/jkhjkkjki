47aaac3445755c799cb0f190198d0c0569ba111e
CHEOLYOUNGCHOI <32664134+CHEOLYOUNGCHOI@users.noreply.github.com>
2017-10-10 17:37:07 +0900

    Add files via upload

---
+++ 과제 최철영 201503082.py
@@ -0,0 +1,228 @@
+import turtle as t
+t.shape("turtle")
+t.up
+t.back(100)
+t.down
+t.pensize(3)
+t.fd(200)
+t.lt(90)
+t.forward(200)
+t.lt(90)
+t.fd(200)
+t.lt(90)
+t.fd(200)
+t.up()
+t.back(150)
+t.lt(90)
+t.fd(30)
+t.down()
+t.fd(43)
+t.up()
+t.fd(30)
+t.down()
+t.fd(43)
+t.rt(90)
+t.up()
+t.fd(15)
+t.rt(90)
+t.fd(20)
+t.down()
+
+t.color("black")
+t.pensize(3)
+t.circle(5)
+
+t.up()
+t.fd(70)
+t.down()
+
+t.color("black")
+t.pensize(3)
+t.circle(5)
+
+t.up()
+t.lt(90)
+t.fd(50)
+t.lt(90)
+t.fd(30)
+t.down()
+
+t.color("black")
+t.pensize(3)
+t.circle(10)
+
+t.up()
+t.fd(30)
+t.down()
+
+t.color("gray")
+t.begin_fill()
+for x in range(10):
+    t.forward(5)
+    t.lt(360/100)
+    t.end_fill()
+
+t.up()
+t.rt(120)
+t.fd(30)
+t.rt(90)
+t.fd(50)
+t.lt(180)
+t.down()
+
+t.color("gray")
+t.begin_fill()
+for x in range(7):
+    t.forward(7)
+    t.lt(360/80)
+    t.end_fill()
+
+t.up()
+t.rt(213)
+t.fd(100)
+t.down()
+
+t.color("gray")
+t.begin_fill()
+for x in range(7):
+    t.forward(7)
+    t.rt(360/80)
+    t.end_fill()
+
+
+t.up()
+t.rt(145)
+t.fd(50)
+t.rt(180)
+t.down()
+
+t.color("gray")
+t.begin_fill()
+for x in range(10):
+    t.forward(5)
+    t.rt(360/100)
+    t.end_fill()
+
+t.up()
+t.lt(150)
+t.fd(60)
+t.rt(90)
+t.lt(140)
+t.down()
+
+t.color("red")
+t.begin_fill()
+for x in range(9):
+    t.forward(9)
+    t.lt(360/100)
+    t.end_fill()
+
+t.up()
+t.rt(160)
+t.down()
+
+t.color("red")
+t.begin_fill()
+for x in range(9):
+    t.forward(9)
+    t.rt(360/40)
+    t.end_fill()
+
+t.color("black")
+t.up()
+t.rt(70)
+t.fd(170)
+t.down()
+t.lt(20)
+t.fd(40)
+t.lt(160)
+t.fd(40)
+
+t.up()
+t.lt(8)
+t.fd(210)
+t.down()
+
+t.fd(80)
+
+t.color("black")
+t.begin_fill()
+for x in range(9):
+    t.forward(9)
+    t.lt(360/8)
+    t.end_fill()
+
+t.up()
+t.lt(100)
+t.fd(120)
+t.rt(113)
+t.down()
+t.fd(80)
+
+t.color("black")
+t.begin_fill()
+for x in range(9):
+    t.forward(9)
+    t.rt(360/8)
+    t.end_fill()
+
+
+t.up()
+t.rt(160)
+t.fd(80)
+t.lt(90)
+t.fd(55)
+t.down()
+
+t.lt(95)
+t.fd(150)
+t.rt(95)
+t.fd(50)
+t.rt(95)
+t.fd(150)
+
+t.up()
+t.rt(175)
+t.fd(150)
+t.down()
+t.fd(30)
+t.rt(95)
+t.fd(10)
+
+t.up()
+t.rt(180)
+t.fd(30)
+t.lt(90)
+t.fd(30)
+t.rt(180)
+
+t.down()
+t.fd(30)
+t.lt(90)
+t.fd(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
